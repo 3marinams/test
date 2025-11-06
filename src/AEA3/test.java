e5647d84829af3ec530c7dac568636482ae76280
marinamoreno <itsmarinams@gmail.com>
2025-11-06 19:11:55 +0100

    hello world :)

---
+++ src/AEA3/test.java
@@ -0,0 +1,12 @@
+package AEA3;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("hola! ¿Cómo te llamas?");
+        System.out.println("Encantada de conocerte " + s.nextLine());
+    }
+    
+}
